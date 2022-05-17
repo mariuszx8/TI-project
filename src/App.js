e77da147e84815db8b8db275be5527c1d8d7b062
mariusz <mariusz.kudlacz@op.pl>
2022-05-17 02:19:56 +0200

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/HomePage/HomePage";
+import { LoginPage } from "./pages/LoginPage/LoginPage";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
