303efb9104569ffdb7949fcb451a10510b8796ad
mariusz <mariusz.kudlacz@op.pl>
2022-05-24 02:16:17 +0200

    Created node js script for handling routing

--- src/server.js
+++ src/server.js
@@ -6,4 +6,8 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "build")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 app.listen(port, () => console.log(`Server is running on port ${port}!`));
